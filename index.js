e41baaeb8ccfb88b492a5ccc201bf5d6875ec264
mdumar112233 <mdumarfaruk187326@gmail.com>
2021-03-27 08:39:13 +0600

    added new code

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const pass = 'arabian-hotem1122';
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://arabian-hotel:arabian-hotem1122@cluster0.ij0ac.mongodb.net/burj-al-arab?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) => {
     res.send('hello');
@@ -11,9 +18,23 @@ app.get('/', (req, res) => {
 
 
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connection');
-  client.close();
+  const bookings = client.db("burj-al-arab").collection("bookings");
+  
+  app.post('/addBooking', (req, res) => {
+      const newBooking = req.body;
+      bookings.insertOne(newBooking)
+      .then(result => {
+        res.send(result.insertedCount > 0);
+        console.log(result);
+      })
+  })
+
+  app.get('/bookings', (req, res) => {
+    bookings.find({email: req.query.email})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
 });
 
 
